762d3dbbba3c80014af22b3303ad8d6ce93e8b1e
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2025-03-14 11:40:15 +0530

    basic intialized

---
+++ common/otp.go
@@ -0,0 +1,18 @@
+package common
+
+
+import (
+	"math/rand"
+	"time"
+)
+
+
+func GenerateOTP(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	const chars = "0123456789"
+	result := make([]byte,length)
+	for i:=0; i<length; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
+}
---
+++ common/user.go
@@ -0,0 +1,49 @@
+package common
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type requestResponse struct {
+	Message string      `json:"message"`
+	Status  uint        `json:"status"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func SuccessResponse(ctx *gin.Context, msg string) {
+	response := requestResponse{
+		Message: msg,
+		Status:  http.StatusOK,
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
+
+func BadResponse(ctx *gin.Context, msg string) {
+	response := requestResponse{
+		Message: msg,
+		Status:  http.StatusOK,
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
+
+func SuccessResponseWithData(ctx *gin.Context, msg string, data interface{}) {
+	response := requestResponse{
+		Message: msg,
+		Status: http.StatusOK,
+		Data: data,
+	}
+	ctx.JSON(http.StatusOK, response)
+}
+
+func InternalServerErrorResponse(ctx *gin.Context, msg string) {
+	response := requestResponse{
+		Message: msg,
+		Status: http.StatusInternalServerError,
+	}
+
+	ctx.JSON(http.StatusInternalServerError, response)
+}
